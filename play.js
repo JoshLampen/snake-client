73b643a35433c385df54e7f55706790edf7c7635
JoshLampen <joshlampen2@gmail.com>
2020-05-05 19:26:04 +0000

    added stdin, specifically 'ctrl + c' to exit

--- play.js
+++ play.js
@@ -1,4 +1,21 @@
 const { connect } = require('./client');
+const stdin = process.stdin;
+
+const handleUserInput = stdin.on('data', key => {
+  if (key === '\u0003') { // if it's ctrl + c, exit the program
+    process.exit();
+  }
+});
+
+const setupInput = () => {
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  handleUserInput;
+  return stdin;
+};
 
 console.log('Connecting ...');
 connect();
+setupInput();

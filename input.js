2b983f60e2e714c3894b989918cf4204e0c6c9b0
JoshLampen <joshlampen2@gmail.com>
2020-05-05 19:49:55 +0000

    user can now move the snake using stdin arguments

--- input.js
+++ input.js
@@ -1,12 +1,24 @@
 const stdin = process.stdin;
 
+let connection;
+
 const handleUserInput = stdin.on('data', key => {
   if (key === '\u0003') { // if it's ctrl + c, exit the program
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'd') {
+    connection.write('Move: right');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'a') {
+    connection.write('Move: left');
   }
 });
 
-const setupInput = () => {
+const setupInput = conn => {
+  connection = conn;
+
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
